a800d0981c82e17381faf402a302e1c8c92809d8
Deepak Mokal <mokaldeepak008@gmail.com>
2026-02-13 02:15:46 +0530

    setup: prisma added and first route task tested

--- src/app.js
+++ src/app.js
@@ -1,9 +1,33 @@
-const express = require('express')
+const express = require('express');
+const prisma = require('./config/prisma');
 
 const app = express();
 
+
+
 app.get('/', (req,res) => {
     res.send("Server is running 🚀")
+})
+
+app.use(express.json())
+
+//create task
+
+app.post('/tasks', async (req, res) => {
+    const {title} = req.body
+
+    const task = await prisma.task.create({
+        data: {
+            title
+        }
+    })
+    res.json(task)
+})
+
+//GET ALL
+app.get('/tasks', async (req, res) => {
+    const tasks = await prisma.task.findMany()
+    res.json(tasks)
 })
 
 
